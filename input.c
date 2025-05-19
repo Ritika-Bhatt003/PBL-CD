1b5649cce61c3d2a93a9a2114625d8f9c37668d2
Ritika Bhatt <112641546+Ritika-Bhatt003@users.noreply.github.com>
2025-05-19 09:14:09 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,40 @@
+int main() {
+    int a;
+    a = 10;
+    // efhowurnf
+
+    /*kfdjvn
+    lfjvnelfk
+    jfv*/
+
+    if (0) {
+        a = 100;
+    } else {
+        a = 200; // ygi
+    }
+
+    if (1) {
+        a = 300;
+    } else {
+        a = 400;
+    }
+
+    if (a) {
+        a = 500;
+    } else {
+        a = 600;
+    }
+
+    while (1) {
+        a = 700;
+        break;
+        a = 800;
+    }
+     while (0) {
+        a = 800;
+    }
+
+
+    return a;
+    a= 900;
+}
---
+++ output.c
@@ -0,0 +1,22 @@
+int main() {
+    int a;
+    a = 10;
+    // efhowurnf
+    /*kfdjvn
+    lfjvnelfk
+    jfv*/
+    a = 200;
+    // ygi
+    a = 300;
+    if (a) {
+    a = 500;
+    } else {
+    a = 600;
+    }
+    while (1) {
+    a = 700;
+    break;
+    }
+    return a;
+}
+
